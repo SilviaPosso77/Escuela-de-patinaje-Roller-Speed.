f1a130144296747e68d6f6ef12a32499e8be9e51
Julian <julian.salamanca@est.iudigital.edu.co>
2025-03-23 13:28:22 -0500

    Se añade horario profesor y se cambia metodo de llamada de la vista sql

--- v1/src/main/java/com/rollerspeed/v1/Controllers/HorariosController.java
+++ v1/src/main/java/com/rollerspeed/v1/Controllers/HorariosController.java
@@ -3,6 +3,7 @@ package com.rollerspeed.v1.Controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rollerspeed.v1.Repository.HorariosEstudiantesRepositorio;
+import com.rollerspeed.v1.Repository.HorariosProfesoresRepositorio;
 
 @Controller
 @RequestMapping("/horarios")
@@ -18,28 +20,86 @@ public class HorariosController {
 
     private final HorariosEstudiantesRepositorio HorariosEstudiantes;
 
+    private final HorariosProfesoresRepositorio HorariosProfesores;
+
     private final JdbcTemplate jdbcTemplate;
 
-    public HorariosController(HorariosEstudiantesRepositorio HorariosEstudiantes,JdbcTemplate jdbcTemplate) {
+    private boolean vistaEstudianteCreada = false;
+    private boolean vistaProfesorCreada = false;
+
+    @Autowired
+    public HorariosController(HorariosEstudiantesRepositorio HorariosEstudiantes,JdbcTemplate jdbcTemplate, HorariosProfesoresRepositorio HorariosProfesores) {
         this.HorariosEstudiantes = HorariosEstudiantes;
         this.jdbcTemplate = jdbcTemplate;
+        this.HorariosProfesores = HorariosProfesores;
     }
 
     
     @PostMapping("/estudiantes")
     public String crearHorariosEstudiantes(Model modelo) {
-        HorariosEstudiantes.crearHorariosEstudiantes();
-        return "Horarios/horariosestudiantes";
+        return obtenerYEnviarHorarios(modelo, "horarios_estudiantes","Horarios/horariosestudiantes");
     }
 
     @GetMapping("/estudiantes")
     public String HorariosEstudiantes(Model modelo) {
-        return obtenerYEnviarHorarios(modelo);
+        return obtenerYEnviarHorarios(modelo, "horarios_estudiantes","Horarios/horariosestudiantes");
     }
 
-    private String obtenerYEnviarHorarios(Model modelo){
-        List<Map<String, Object>> resultados = jdbcTemplate.queryForList("SELECT * FROM horarios_estudiantes");
-        modelo.addAttribute("resultados", resultados);
-        return "Horarios/horariosestudiantes";
+    private String obtenerYEnviarHorarios(Model modelo, String Vista, String Ruta){
+        try{
+            if(!vistaEstudianteCreada){
+                if(!vistaExiste(Vista)){
+                    HorariosEstudiantes.crearHorariosEstudiantes();
+                }
+                vistaProfesorCreada = true;
+            }
+            List<Map<String, Object>> resultados = jdbcTemplate.queryForList("SELECT * FROM horarios_estudiantes");
+            modelo.addAttribute("resultados", resultados);
+            return Ruta;
+        }catch( Exception e){
+            modelo.addAttribute("Error", "No funciona: "+e.getMessage());
+            return "error";
+        }
+        
     }
+
+    private boolean vistaExiste(String Vista){
+        try{
+            jdbcTemplate.queryForObject("Select 1 from "+Vista+" limit 1", Integer.class);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+      
+    @PostMapping("/profesores")
+    public String crearHorariosProfesores(Model modelo) {
+        return obtenerYEnviarHorariosProfesores(modelo, "horarios_estudiantes","Horarios/horariosprofesores");
+    }
+
+    @GetMapping("/profesores")
+    public String HorariosProfesores(Model modelo) {
+        return obtenerYEnviarHorariosProfesores(modelo, "horarios_estudiantes","Horarios/horariosprofesores");
+    }
+
+    private String obtenerYEnviarHorariosProfesores(Model modelo, String Vista, String Ruta){
+        try{
+            if(!vistaProfesorCreada){
+                if(!vistaExiste(Vista)){
+                    HorariosProfesores.crearHorariosProfesores();
+                }
+                vistaProfesorCreada = true;
+            }
+            List<Map<String, Object>> resultados = jdbcTemplate.queryForList("SELECT * FROM horarios_profesores");
+            modelo.addAttribute("resultados", resultados);
+            return Ruta;
+        }catch( Exception e){
+            modelo.addAttribute("Error", "No funciona: "+e.getMessage());
+            return "error";
+        }
+        
+    }
+    
+
 }
