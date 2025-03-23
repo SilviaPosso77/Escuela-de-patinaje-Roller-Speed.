afe531ec55b8cab9de9d23d409de0eaf372adbd2
Silvia Posso <silviapossomazo@gmail.con>
2025-03-22 19:44:30 -0500

    Formulario

--- v1/src/main/java/com/rollerspeed/v1/Service/ClaseService.java
+++ v1/src/main/java/com/rollerspeed/v1/Service/ClaseService.java
@@ -1,9 +1,8 @@
 package com.rollerspeed.v1.Service;
 
-
+import com.rollerspeed.v1.Model.Clase;
 import java.util.List;
 
-import com.rollerspeed.v1.Model.Clase;
 
 public interface ClaseService {
     public List<Clase> listarTodasLasClases();
