67752b43de2de70332271e25b10801aa22c7679c
Julian <julian.salamanca@est.iudigital.edu.co>
2025-03-23 13:29:47 -0500

    Se elimian elementos que ya no se van a usar

--- v1/src/main/java/com/rollerspeed/v1/Service/HorarioProfesorServicio.java
+++
@@ -1,9 +0,0 @@
-package com.rollerspeed.v1.Service;
-
-import java.util.List;
-
-import com.rollerspeed.v1.Model.HorarioProfesor;
-
-public interface HorarioProfesorServicio {
-    public List<HorarioProfesor> mostrarHorariosProfesores();
-}
