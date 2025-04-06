16bfaafc7b795db39ac60e5d670f5ae7c1ff7509
Julian <julian.salamanca@est.iudigital.edu.co>
2025-04-06 11:22:06 -0500

    Modificacion para guardar estudiante

--- v1/src/main/java/com/rollerspeed/v1/Service/EstudianteService.java
+++ v1/src/main/java/com/rollerspeed/v1/Service/EstudianteService.java
@@ -1,12 +1,24 @@
 package com.rollerspeed.v1.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rollerspeed.v1.DTO.EstudianteDTO;
+import com.rollerspeed.v1.Model.ERole;
 import com.rollerspeed.v1.Model.Estudiante;
+import com.rollerspeed.v1.Model.RolEntity;
+import com.rollerspeed.v1.Model.UserEntity;
+import com.rollerspeed.v1.Model.Clase;
+import com.rollerspeed.v1.Repository.ClaseRepositorio;
 import com.rollerspeed.v1.Repository.EstudianteRepository;
+import com.rollerspeed.v1.Repository.RoleRespositorio;
+import com.rollerspeed.v1.Repository.UserRespositorio;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class EstudianteService {
@@ -14,12 +26,50 @@ public class EstudianteService {
     @Autowired
     private EstudianteRepository estudianteRepository;
 
+    @Autowired
+    private UserRespositorio userRespositorio;
+
+    @Autowired
+    private RoleRespositorio roleRespositorio;
+
+    @Autowired
+    private ClaseRepositorio claseRepositorio;
+
     public List<Estudiante> listarEstudiantes() {
         return estudianteRepository.findAll();
     }
 
-    public void registrarEstudiante(Estudiante estudiante) {
+    @Transactional
+    public void registrarEstudiante(EstudianteDTO estudianteDTO) {
+
+        Clase clase = claseRepositorio.findById(estudianteDTO.getIdClase()).get();
+
+        Estudiante estudiante = Estudiante.builder()
+                                .id(estudianteDTO.getId())
+                                .nombre(estudianteDTO.getNombre())
+                                .apellido(estudianteDTO.getApellido())
+                                .fechaNacimiento(estudianteDTO.getFechaNacimiento())
+                                .genero(estudianteDTO.getGenero())
+                                .clase(clase)
+                                .build();
+        UserEntity user = UserEntity.builder()
+                        .username(estudiante.getEmail()) //Asigna el email como nombre de usuario
+                        .password(estudianteDTO.getContraseña())
+                        .roles(obtenerRol())
+                        .build();
+                
+        userRespositorio.save(user); //Guarda el usuario
+        estudiante.setUser(user); //Relaciona el usuario con el estudiante
         estudianteRepository.save(estudiante);
+    }
+
+    private Set<RolEntity> obtenerRol(){
+        //Logica para asignar el rol de Estudiante
+        RolEntity rolEstudiante = roleRespositorio.findByName(ERole.ESTUDIANTE)
+        .orElseThrow(()-> new RuntimeException("Rol ESTUDIANTE no encontrado")); //En caso de que el Rol Estudiante no exista
+        Set<RolEntity> roles = new HashSet<>();
+        roles.add(rolEstudiante);
+        return roles;
     }
     
     public Estudiante ObtenerId(Long Id){
